bc16b7bf754b738d35fba50868a4ec60b4f366c7
SatoShun <shun.sato1@gmail.com>
2014-11-29 15:46:17 +0000

    use etc init.d mysql

--- start.bash
+++ start.bash
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-mysql.server start
-
 if [ ! -d "/var/lib/mysql/performance_schema" ]; then
     mysql_install_db --datadir=/var/lib/mysql
 fi
+
+/etc/init.d/mysql start
 
 mysqladmin --silent --wait=30 ping || exit 1
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'
 mysql -uroot -e 'CREATE DATABASE if not exists blog CHARACTER SET utf8 COLLATE utf8_general_ci;'
 
-mysql.server stop
+/etc/init.d/mysql stop
 
 
 mysqld_safe
