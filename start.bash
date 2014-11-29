1fa794fa14c99284c97216360d76c361cc60b11c
SatoShun <shun.sato1@gmail.com>
2014-11-29 14:35:21 +0000

    sleep

--- start.bash
+++ start.bash
@@ -2,6 +2,8 @@
 
 mysqld_safe &
 pid=$!
+sleep 3
+
 if [ ! -d "/var/lib/mysql/performance_schema" ]; then
     mysql_install_db --datadir=/var/lib/mysql
 fi
@@ -9,6 +11,6 @@ fi
 mysqladmin --silent --wait=30 ping || exit 1
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'
 mysql -uroot -e 'CREATE DATABASE if not exists blog CHARACTER SET utf8 COLLATE utf8_general_ci;'
-kill $pid
+kill -KILL $pid
 
 mysqld_safe
